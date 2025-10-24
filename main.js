aa954a8b08c5df82a8e2460f3cad69fecde76794
Seba-Filippa-934 <filippa.seba@gmail.com>
2025-10-24 12:08:00 -0300

    solo estan terminadas la primera pagina, y la de canton

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-//hacer algo minimo
+function muestra_oculta(id) {
+    const div = document.getElementById(id);
+    if (div.style.display === "none" || div.style.display === "") {
+        div.style.display = "flex";
+    } else {
+        div.style.display = "none";
+    }
+}
